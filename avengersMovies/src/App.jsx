388c3423ba86616e7bad08ef30eba591e45df25f
Daniel Henrique de Carvalho <105288296+danielcta@users.noreply.github.com>
2023-09-14 10:57:30 -0300

    Update App.jsx

--- avengersMovies/src/App.jsx
+++ avengersMovies/src/App.jsx
@@ -5,7 +5,11 @@ import { useEffect } from 'react'
 import { Route } from 'react-router-dom'
 
 function App() {
-    <Card />
+            <div>
+                <h3>{movieName}</h3>
+                <h3>{movie}h3>
+                <h3>Dimensão: {dim}</h3>
+            </div>
 }
 
 export default App
