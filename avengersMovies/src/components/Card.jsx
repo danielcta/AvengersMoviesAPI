7a2b14447d4aa7b320c2c50774e1e55eccea1134
Daniel Henrique de Carvalho <105288296+danielcta@users.noreply.github.com>
2023-09-14 11:35:49 -0300

    Update Card.jsx

--- avengersMovies/src/components/Card.jsx
+++ avengersMovies/src/components/Card.jsx
@@ -2,49 +2,16 @@ import { useEffect, useState } from 'react'
 import styles from './card.module.css'
 import '../App.jsx'
 
-function Card() {
-
-   const [ movies, setMovies ] = useState([])
-
-useEffect(() => {
-    fetch('https://github.com/danielcta/AvengersMovies/edit/main/avengersAPI/avengers_movies.json')
-      .then(response => response.json())
-      .then(data => {
-        setCharacters(data.results);
-      })
-      .catch(error => {
-        console.error('Error fetching characters:', error);
-      });
-  }, []);
-   
-    return (
-        <>
-            <section>
-                <h2>Avengers Movies</h2>
-                {
-                    movies.length > 0 ? (
-                        <section>
-                            {
-                                movies.map((movie) => (
-                                    <Card
-                                        moviePoster = {movie.poster}
-                                        movieName = {movie.name}
-                                        releaseYear = {movie.releaseYear}
-                                        movieSinopse = {movie.sinopse}
-                                        mainActors = {movie.mainActors}
-                                        />
-                                ))
-                            }
-                        </section>
-                    ) : (
-                        <p>Carregando Filmes</p>
-                    )
-                }     
-                
-            </section>
-
-        </>
-    )
+function Card({ moviePoster, movieName, releaseYear, movieSinopse, mainActors }) {
+  return (
+    <div className="movie-card">
+      <img src={moviePoster} alt={movieName} />
+      <h3>{movieName}</h3>
+      <p>Year: {releaseYear}</p>
+      <p>Sinopse: {movieSinopse}</p>
+      <p>Main Actors: {mainActors.join(', ')}</p>
+    </div>
+  );
 }
 
 export default Card
