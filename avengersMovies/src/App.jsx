5bc9b09b18f3268a5ba0530ed380ed288f59dd62
Daniel Henrique de Carvalho <105288296+danielcta@users.noreply.github.com>
2023-09-14 10:44:24 -0300

    Update App.jsx

--- avengersMovies/src/App.jsx
+++ avengersMovies/src/App.jsx
@@ -4,35 +4,8 @@ import './App.css'
 import { useEffect } from 'react'
 import { Route } from 'react-router-dom'
 
-function App() 
-const [ movies, setMovies ] = useState([])
-
-useEffect(() => {
-    fetch('https://github.com/danielcta/AvengersMovies/edit/main/avengersAPI/avengers_movies.json')
-      .then(response => response.json())
-      .then(data => {
-        setCharacters(data.results);
-      })
-      .catch(error => {
-        console.error('Error fetching characters:', error);
-      });
-  }, []);
-
-
-{  
-return(
-<Route>
-<Route path="/poster" element={<moviePoster movies={movies} />} />
-<Route path="/name" element={<movieName movies={movies} />} />
-<Route path="/releaseYear" element={<releaseYear movies={movies} />} />
-<Route path="/sinopse" element={<movieSinopse movies={movies} />} />
-<Route path="/mainActors" element={<mainActors movies={movies} />} />
-</Route>
-)
-
-
-
-
+function App() {
+    <Card />
 }
 
 export default App
