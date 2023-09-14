da6a1aa11a9ecc94f56342e4269e143ea427ee56
Daniel Henrique de Carvalho <105288296+danielcta@users.noreply.github.com>
2023-09-14 10:59:53 -0300

    Update App.jsx

--- avengersMovies/src/App.jsx
+++ avengersMovies/src/App.jsx
@@ -6,9 +6,11 @@ import { Route } from 'react-router-dom'
 
 function App() {
             <div>
+                <img src={moviePoster}></img>
                 <h3>{movieName}</h3>
-                <h3>{movie}h3>
-                <h3>Dimensão: {dim}</h3>
+                <h3>{releaseYear}h3>
+                <h3>{sinopse}</h3>
+                <h3>{mainActors}</h3>    
             </div>
 }
 
